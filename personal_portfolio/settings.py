367acb02db6acdcde70125f4ea25f73f281e41a2
“josh <cian79421@hotmail.com>
2021-05-19 07:27:01 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bgc(#mo#khjdsr(2%f-j(ty**rbn-*0m6w_ql&mv0g7k(9^j*='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cian421.pythonanywhere.com']
 
 
 # Application definition
@@ -120,12 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = (BASE_DIR / 'static')
 
 MEDIA_URL = '/media/'
-
+MEDIA_ROOT = (BASE_DIR / 'media')
 # 以前寫法會寫 MEDIA_ROOT = os.path.join(BASE_DIR / 'media'),現在寫下面這種方式即可
 
-MEDIA_ROOT = (BASE_DIR / 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
