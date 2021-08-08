7eb8dd727655026709dd568354529e4cf7bce138
Joshyu421 <cian79421@hotmail.com>
2021-08-09 01:43:05 +0800

    change setting

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -121,15 +121,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = (BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = (BASE_DIR / 'media')
+# MEDIA_ROOT = (BASE_DIR / 'media')
 
-STATICFILES_DIRS = os.path.join[
-    BASE_DIR /  'static',
-    BASE_DIR / 'portfolio/build/static'
-]
+
+# STATICFILES_DIRS = [
+#     BASE_DIR /  'static',
+#     BASE_DIR / 'portfolio/build/static'
+# ]
+
+STATICFILES_DIRS =  (os.path.join(BASE_DIR, 'static'),)
 
 # 以前寫法會寫 MEDIA_ROOT = os.path.join(BASE_DIR / 'media'),現在寫下面這種方式即可
 
